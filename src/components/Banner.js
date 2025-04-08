9c02c4ad52b90cb938ac3a98e631905d6fbc371f
“ADRIENNE <adrienneklee@gmail.com>
2025-04-08 15:00:22 -0400

    added banner section

---
+++ src/components/Banner.js
@@ -0,0 +1,60 @@
+import { useState, useEffect } from "react"
+import { Container, Row, Col } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import HeaderImg from "../assets/img/header-img.jpg"
+
+export const Banner = () => {
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [text, setText] = useState('');
+    const [delta, setDelta] = useState(300 - Math.random()*100);
+    const toRotate = ["Senior Web Specialist", "Web Developer", "React Developer"];
+    const period = 2000;
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        }, delta)
+
+        return () => { clearInterval(ticker)};
+    }, [text])
+
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+        setText(updatedText);
+
+        if (isDeleting) {
+            setDelta(preDelta => preDelta /2)
+        }
+        if (!isDeleting && updatedText === fullText) {
+            setIsDeleting(true);
+            setDelta(period);
+        } else if (isDeleting && updatedText === '') {
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setDelta(500);
+        }
+    }
+
+    return (
+        <section className="banner" id="home">
+            <Container>
+                <Row className="align-items-center">
+                    <Col xs={12} md={6} xl={7}>
+                    <span className="tagline">Welcome to my Portfolio</span>
+                    <h1>{`Hi I'm Adrienne, `}<span className="wrap">{text}</span> </h1>
+                    <p>A Full Stack Developer based in Hamilton, Ontario, and the sole Web Developer at Point Blank Creative - Canada's first unionized Ad Agency. I love being able to bridge creativity and tech to build dynamic and meaningful web applications.</p>
+                    <button onClick={()=>console.log('connect')}>Let's connect <ArrowRightCircle size={25}/> </button>
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+                        <img src={HeaderImg} alt="Header Img"/>
+                    </Col>
+                </Row>
+            </Container>
+
+        </section>
+    )
+}
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -41,9 +41,9 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
                 <div className="social-icon">
-                    <a href="#"><img src={navIcon1} alt="" /></a>
-                    <a href="#"><img src={navIcon2} alt="" /></a>
-                    <a href="#"><img src={navIcon3} alt="" /></a>
+                    <a href="https://www.linkedin.com/in/adrienneklee/" target="_blank" rel="noopener noreferrer"><img src={navIcon1} alt="LinkedIn logo" /></a>
+                    <a href="https://github.com/adrienneklee" target="_blank" rel="noopener noreferrer"><img src={navIcon2} alt="Github logo" /></a>
+                    <a href="https://x.com/adrienneklee/" target="_blank" rel="noopener noreferrer"><img src={navIcon3} alt="X logo" /></a>
                 </div>
                 <button className="vvd" onClick={()=> console.log('connect')}><span>Let's Connect</span></button>
             </span>
